05bf06f086bec72ed0a9dc9115a1a12316ad871e
Ice Lam <pinky.yy.lam@gmail.com>
2020-10-18 19:31:36 +0800

    test: add test for throttle utils

---
+++ src/utils/__test__/throttle.test.js
@@ -0,0 +1,30 @@
+import throttle from '../throttle';
+
+jest.useFakeTimers();
+
+const functionToBeThrottled = jest.fn(() => {});
+
+describe('throttle()', () => {
+  it('should apply throttle to function', () => {
+    const throttledFunction = jest.fn(throttle(functionToBeThrottled, 500));
+
+    expect(throttledFunction).toHaveBeenCalledTimes(0);
+    expect(functionToBeThrottled).toHaveBeenCalledTimes(0);
+
+    // Trigger throttledFunction for 2 times
+    throttledFunction();
+    throttledFunction();
+    expect(throttledFunction).toHaveBeenCalledTimes(2);
+    expect(functionToBeThrottled).toHaveBeenCalledTimes(1);
+
+    // Stimulate time pass
+    jest.runOnlyPendingTimers();
+
+    // Trigger throttledFunction for 3 times again
+    throttledFunction();
+    throttledFunction();
+    throttledFunction();
+    expect(throttledFunction).toHaveBeenCalledTimes(5);
+    expect(functionToBeThrottled).toHaveBeenCalledTimes(2);
+  });
+});
