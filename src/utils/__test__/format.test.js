fb84e7297fb4452a7f2f4ef16a7db9c78034ad29
Ice Lam <pinky.yy.lam@gmail.com>
2020-10-18 18:47:01 +0800

    test: add test for format utils

---
+++ src/utils/__test__/format.test.js
@@ -0,0 +1,24 @@
+import { zeroPad, formatDate } from '../format';
+
+const ISODateString = '2020-10-18T09:53:34.140Z';
+
+describe('zerpPad()', () => {
+  it('should append leading zero to integer less than 10', () => {
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < 10; i++) {
+      expect(zeroPad(i)).toEqual(`0${i}`);
+    }
+  });
+
+  it('should append not leading zero to integer greater than or equal to 10', () => {
+    expect(zeroPad(10)).toEqual('10');
+    expect(zeroPad(33)).toEqual('33');
+    expect(zeroPad(65)).toEqual('65');
+  });
+});
+
+describe('formatDate()', () => {
+  it('should convert ISO date string to "YYYY-MM-DD HH:mm" format', () => {
+    expect(formatDate(ISODateString)).toEqual('2020-10-18 17:53');
+  });
+});
